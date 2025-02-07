1255ccb9cdb3a8bfb07dc9464802e89ed527aa19
Imtiyaj <imti5791@gmail.com>
2025-02-07 19:48:10 +0530

    new

--- Terraform depedency/main.tf
+++ Terraform depedency/main.tf
@@ -20,4 +20,10 @@ resource "aws_instance" "server" {
   ami = "ami-0b7207e48d1b6c06f"
   instance_type = "t2.micro"
   depends_on = [ aws_security_group.mysg ]
+
+  lifecycle {
+    prevent_destroy = true  # No one destroy
+    create_before_destroy = true # first create then destroy if we do any changes
+    replace_triggered_by = [ aws_security_group.mysg,aws_security_group.mysg.ingress ] # create new instance if we do changes in SG
+  }
 }
